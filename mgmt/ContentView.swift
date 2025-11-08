68ba3108c00f63379f3b143e9e0542a23d8f42be
AnaKx <anak@aramoni.net>
2025-11-08 22:03:51 +0100

    Initial Commit

---
+++ mgmt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mgmt
+//
+//  Created by Ana Karina Aramoni Ruiz on 08/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mgmt/mgmtApp.swift
@@ -0,0 +1,17 @@
+//
+//  mgmtApp.swift
+//  mgmt
+//
+//  Created by Ana Karina Aramoni Ruiz on 08/11/25.
+//
+
+import SwiftUI
+
+@main
+struct mgmtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
